d0c624e6ce0f3cc7026dd1a412300dd344b843a9
chen <chtunsw@gmail.com>
2019-06-05 17:14:43 +1000

    merge changes

--- model/employee.js
+++ model/employee.js
@@ -11,20 +11,12 @@ const employeeSchema = new Schema({
   //_id: Schema.Types.ObjectId,
   name: String,
   title: String,
-<<<<<<< HEAD
-  portrait: String,
-  password: String,
-  email: String,
-  salary: String,
-  schedules: [{type: Schema.Types.ObjectId, ref:'schedule'}]
-=======
   // potrait: String,
   // password: String,
   // email: String,
   // salary: String,
   // schedules: [{ type: Schema.Types.ObjectId, ref: 'Schedule' }],
   // leaveRequests: [{ type: Schema.Types.ObjectId, ref: 'LeaveRequest' }],
->>>>>>> 7d38a6fbab64e1f6746e72622d9ce7a495f7d0e7
 });
 
 // create employee model 
