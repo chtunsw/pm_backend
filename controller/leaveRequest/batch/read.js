141e32e0badf565da02b979852311bd88d727902
chen <chtunsw@gmail.com>
2019-08-29 10:45:19 +1000

    remove console.log

--- controller/leaveRequest/batch/read.js
+++ controller/leaveRequest/batch/read.js
@@ -8,10 +8,8 @@ module.exports = async (req, res, next) => {
         var { id, pageIndex, pageSize } = req.params;
         pageIndex = Number(pageIndex)
         pageSize = Number(pageSize)
-        console.log(id, pageIndex, pageSize)
         if (ObjectId.isValid(id)) {
             const result = await leaveRequest.find({ employeeID: id }, undefined, { limit: pageSize, skip: pageIndex * pageSize })
-            console.log(result.length)
             if (result.length) {
                 res.statusCode = 200;
                 res.send({
